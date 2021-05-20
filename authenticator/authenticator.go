986ee91cc98b6c69381584fbe28cfead5e51dcb9
Nikolai <npv3s@skopa.dev>
2021-05-20 10:39:36 +0300

    Добавлен список админов, переделана генерация сессионного токена

--- authenticator/authenticator.go
+++ authenticator/authenticator.go
@@ -5,8 +5,19 @@ import (
 	"errors"
 	"golang.org/x/crypto/bcrypt"
 	"main/database"
+	"math/rand"
 	"strconv"
 )
+
+const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+func randStringBytes(n int) []byte {
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = letterBytes[rand.Intn(len(letterBytes))]
+	}
+	return b
+}
 
 type sessions map[int]hash
 
@@ -15,19 +26,35 @@ type hash []byte
 type Authenticator struct {
 	database interface{ database.Database }
 	sessions sessions
+	admins []int
 }
 
-func NewAuthenticator(database database.Database) Authenticator {
-	return Authenticator{
+func NewAuthenticator(database database.Database) (*Authenticator, error) {
+	admins, err := database.GetAdmins()
+	if err != nil {
+		return nil, err
+	}
+
+	return &Authenticator{
 		database,
 		sessions{},
-	}
+		admins,
+	}, nil
 }
 
-func (a *Authenticator) NewUser(login, password string) (*int, error) {
+func (a *Authenticator) GenPassword(password string) ([]byte, error) {
 	hashBytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
 	if err != nil {
 		return nil, errors.New("Hash gen error: " + err.Error())
+	}
+
+	return hashBytes, nil
+}
+
+func (a *Authenticator) NewUser(login, password string) (*int, error) {
+	hashBytes, err := a.GenPassword(password)
+	if err != nil {
+		return nil, err
 	}
 	return a.database.NewUser(login, string(hashBytes))
 }
@@ -52,12 +79,12 @@ func (a *Authenticator) GetSession(userId int) (string, error) {
 		return string(token), nil
 	}
 
-	passwd, err := a.database.GetUserById(userId)
+	_, err := a.database.GetUserById(userId)
 	if err != nil {
 		return "", errors.New("No user with id " + strconv.Itoa(userId))
 	}
 
-	hashBytes, err := bcrypt.GenerateFromPassword([]byte(passwd.PassHash), 14)
+	hashBytes, err := bcrypt.GenerateFromPassword(randStringBytes(16), 5)
 	if err != nil {
 		return "", errors.New("Hash gen error: " + err.Error())
 	}
@@ -73,5 +100,14 @@ func (a *Authenticator) CheckSession(userId int, token string) bool {
 		return bytes.Equal(rightHash, []byte(token))
 	}
 
+	return false
+}
+
+func (a *Authenticator) IsAdmin(userId int) bool {
+	for _, uId := range a.admins {
+		if uId == userId {
+			return true
+		}
+	}
 	return false
 }
